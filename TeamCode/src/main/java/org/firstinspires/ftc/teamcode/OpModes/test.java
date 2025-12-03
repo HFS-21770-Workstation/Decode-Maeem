600eb0392c5148741a1e669cff2c15c92397191e
HFS-21770-Workstation <roboteam@hfs.school>
2025-12-03 18:28:43 +0200

    Added ball count to the Intake, and used test to wait

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/test.java
@@ -1,35 +1,85 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+
+import static android.os.SystemClock.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.RobotBetSystems.Intake;
 
 @TeleOp
 public class test extends OpMode {
-    DistanceSensor SensorA;
-    DistanceSensor SensorB;
-    double SensorADis;
-    double SensorBDis;
-    double MinDis;
-    final int Dis = 10;
-
+    Intake intake;
+    boolean opModeIsActive;
     @Override
     public void init() {
-        SensorA = hardwareMap.get(DistanceSensor.class, "SensorA");
-        SensorB = hardwareMap.get(DistanceSensor.class, "SensorB");
 
+    intake = new Intake(hardwareMap);
+    }
+    @Override
+    public void start(){
+        opModeIsActive = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (opModeIsActive){
+                    sleep(500);
+                    telemetry.addData("balls capacity:",  intake.UpdateBallCount());
+                }
+
+            }
+        }).start();
     }
 
     @Override
     public void loop() {
-        SensorADis = SensorA.getDistance(DistanceUnit.INCH);
-        SensorBDis = SensorB.getDistance(DistanceUnit.INCH);
-        MinDis = (Dis - SensorADis - SensorBDis) + 2.5;
-        telemetry.addData("SensorA", SensorADis);
-        telemetry.addData("SensorB", SensorBDis);
-        telemetry.addData("MinDis", MinDis);
 
+
+//        telemetry.addData("balls capacity:",  intake.UpdateBallCount());
+//        SensorADis = SensorA.getDistance(DistanceUnit.INCH);
+//        SensorBDis = SensorB.getDistance(DistanceUnit.INCH);
+//        MinDis = (Dis - SensorADis - SensorBDis) + 2.5;
+//        telemetry.addData("SensorA", SensorADis);
+//        telemetry.addData("SensorB", SensorBDis);
+//        telemetry.addData("MinDis", MinDis);
+
+//        if (MinDis > 4.5 && MinDis < 5.3)
+//        {
+//            MaxBalls = 1;
+//            telemetry.update();
+//        }
+//        else if (MinDis > 0 && MinDis < 0.5)
+//        {
+//            MaxBalls = 1;
+//            telemetry.update();
+//        }
+//        else if (MinDis > 10 && MinDis < 10.6)
+//        {
+//            MaxBalls = 2;
+//            telemetry.update();
+//        }
+//        else if (MinDis > 5.5 && MinDis < 6)
+//        {
+//            MaxBalls = 2;
+//            telemetry.update();
+//        }
+//        else if (MinDis > 1.3 && MinDis < 1.6)
+//        {
+//            MaxBalls = 2;
+//            telemetry.update();
+//        }
+//        else if (MinDis < 0)
+//        {
+//            MaxBalls = 0;
+//            telemetry.update();
+//        }
+    }
+
+    @Override
+    public void stop(){
+        opModeIsActive = false;
     }
 }
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBetSystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBetSystems/Intake.java
@@ -1,21 +1,82 @@
 package org.firstinspires.ftc.teamcode.RobotBetSystems;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Intake {
+//    static Intake intake;
 
-    DcMotor intake;
+//    DcMotor intakeMotor;
+    DistanceSensor SensorA;
+    DistanceSensor SensorB;
+    double SensorADis;
+    double SensorBDis;
+    double MinDis;
+    final int Dis = 10;
+    int BallCount = 0;
+    int LastBallDetection = 0;
 
-    private Intake(HardwareMap hardwareMap){
-        intake = hardwareMap.dcMotor.get("intake");
+    public Intake(HardwareMap hardwareMap){
+//        intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
+        SensorA = hardwareMap.get(DistanceSensor.class, "SensorA");
+        SensorB = hardwareMap.get(DistanceSensor.class, "SensorB");
     }
 
-     public void startIntake(){
-            intake.setPower(1);
+//     public void startIntake(){
+//            intakeMotor.setPower(1);
+//     }
+//     public void stopIntake(){
+//            intakeMotor.setPower(0);
+//     }
+     public void RestartBallCount(){
+        BallCount = 0;
      }
-     public void stopIntake(){
-            intake.setPower(0);
+     public int UpdateBallCount(){
+         SensorADis = SensorA.getDistance(DistanceUnit.INCH);
+         SensorBDis = SensorB.getDistance(DistanceUnit.INCH);
+         MinDis = (Dis - SensorADis - SensorBDis) + 2.5;
+
+         if (MinDis > 4.5 && MinDis < 5.3)
+         {
+             if(LastBallDetection == 0){
+                 BallCount += 1;
+                 LastBallDetection = 1;
+             }
+
+         }
+         else if (MinDis > 0 && MinDis < 0.5)
+         {
+             BallCount += 1;
+         }
+         else if (MinDis > 10 && MinDis < 10.6)
+         {
+             BallCount += 2;
+         }
+         else if (MinDis > 5.5 && MinDis < 6)
+         {
+             BallCount += 2;
+         }
+         else if (MinDis > 1.3 && MinDis < 1.6)
+         {
+             BallCount += 2;
+         }
+//         if (BallCount >= 3) new
+//         {
+//             BallCount = 0;
+//         }
+         else if (MinDis < 0)
+         {
+             BallCount += 0;
+         }
+         return BallCount;
      }
+//    static public Intake getInstance(HardwareMap hardwareMap){
+//        if(intake == null){
+//            intake = new Intake(hardwareMap);
+//        }
+//        return intake;
+//    }
 }
